617b9c650c2df1d6900c8706b1ac798b1640d950
nowiwr01w <nowiwr01p@icloud.com>
2024-03-30 14:57:41 +0400

    [Navigation] Move to composeApp module

---
+++ composeApp/src/iosMain/kotlin/platform/navigation/MainComponent.ios.kt
@@ -0,0 +1,8 @@
+package platform.navigation
+
+import com.arkivanov.decompose.ComponentContext
+import navigation.MainNavigatorImpl
+
+actual fun getMainComponent(context: ComponentContext): MainNavigatorImpl {
+    TODO("Not yet implemented")
+}
