5def6e51d56ba3afd4bd44750b176ba6296d3ff7
nowiwr01w <nowiwr01p@icloud.com>
2024-03-30 23:57:41 +0400

    [Navigation] Desktop

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,9 +1,11 @@
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import com.arkivanov.decompose.DefaultComponentContext
+import com.arkivanov.essenty.lifecycle.LifecycleRegistry
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication, title = "DailyDoctor") {
-        App()
+        val defaultComponentContext = DefaultComponentContext(lifecycle = LifecycleRegistry())
+        App(defaultComponentContext)
     }
 }
