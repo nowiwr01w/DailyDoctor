67693f28a553b7622ab9f5baebae76ba3e8e37bd
nowiwr01w <nowiwr01p@icloud.com>
2024-04-01 22:45:19 +0400

    [Auth, Mobile] Basic UI - 1

--- composeApp/src/commonMain/kotlin/ui/mobile/auth/AuthMainScreenMobile.kt
+++ composeApp/src/commonMain/kotlin/ui/mobile/auth/AuthMainScreenMobile.kt
@@ -1,23 +1,29 @@
 package ui.mobile.auth
 
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import base.theme.CustomTheme.colors
 import base.view_model.EffectObserver
 import base.view_model.rememberViewModel
 import dailydoctor.composeapp.generated.resources.Res
-import dailydoctor.composeapp.generated.resources.ic_pin
+import dailydoctor.composeapp.generated.resources.ic_login
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 import ui.common.auth.AuthContract.Effect.NavigateToPrivacyPolicyInfo
@@ -78,13 +84,43 @@ private fun AuthMainScreenContent(
     state: State,
     listener: Listener
 ) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
+    ConstraintLayout(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Red)
+            .background(colors.backgroundColors.grayBackgroundColor)
     ) {
-        Icon()
+        val (icon, inputFieldsContainer) = createRefs()
+
+        val isKeyboardOpen = false // TODO: expect function for iOS
+        val authContentTransitionDp by animateDpAsState(
+            targetValue = if (isKeyboardOpen) 8.dp else 160.dp
+        )
+
+        val iconModifier = Modifier
+            .size(96.dp)
+            .clip(CircleShape)
+            .constrainAs(icon) {
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+                top.linkTo(parent.top, 32.dp)
+            }
+        Icon(modifier = iconModifier)
+
+        val authContentModifier = Modifier
+            .fillMaxWidth()
+            .padding(top = authContentTransitionDp)
+            .clip(RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp))
+            .background(colors.backgroundColors.whiteBackgroundColor)
+            .constrainAs(inputFieldsContainer) {
+                top.linkTo(parent.top)
+                bottom.linkTo(parent.bottom)
+                height = Dimension.fillToConstraints
+            }
+        AuthContent(
+            state = state,
+            listener = listener,
+            modifier = authContentModifier
+        )
     }
 }
 
@@ -93,13 +129,27 @@ private fun AuthMainScreenContent(
  */
 @OptIn(ExperimentalResourceApi::class)
 @Composable
-private fun Icon() = Image(
-    painter = painterResource(Res.drawable.ic_pin),
+private fun Icon(modifier: Modifier) = Image(
+    painter = painterResource(Res.drawable.ic_login),
     contentDescription = "",
-    modifier = Modifier
-        .padding(top = 16.dp)
-        .size(96.dp)
+    modifier = modifier
 )
+
+/**
+ * AUTH CONTENT
+ */
+@Composable
+private fun AuthContent(
+    state: State,
+    listener: Listener,
+    modifier: Modifier
+) {
+    Column(
+        modifier = modifier
+    ) {
+
+    }
+}
 
 /**
  * INPUT FIELDS CONTAINER
