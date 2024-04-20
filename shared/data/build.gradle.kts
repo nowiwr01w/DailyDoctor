bf750b97d036107fcccb4fb57ab0e4aac5b0a263
nowiwr01w <nowiwr01p@icloud.com>
2024-04-20 17:10:48 +0400

    Add data module

---
+++ shared/data/build.gradle.kts
@@ -0,0 +1,52 @@
+import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.kotlinSerialization)
+}
+
+kotlin {
+    applyDefaultHierarchyTemplate()
+
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        browser {
+
+        }
+    }
+
+    jvm("desktop")
+
+    androidTarget {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "11"
+            }
+        }
+    }
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "shared.data"
+        }
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+            }
+        }
+    }
+}
+
+android {
+    namespace = "nowiwr01p.daily.doctor.shared.data"
+    compileSdk = libs.versions.android.targetSdk.get().toInt()
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+}
