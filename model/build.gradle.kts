eb4b4ef5e72cc9eb12c6ccfd96ead2ac36ae4960
nowiwr01w <nowiwr01p@icloud.com>
2024-04-20 17:56:36 +0400

    Fix gradle files

--- model/build.gradle.kts
+++ model/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.androidLibrary)
     alias(libs.plugins.kotlinSerialization)
 }
 
