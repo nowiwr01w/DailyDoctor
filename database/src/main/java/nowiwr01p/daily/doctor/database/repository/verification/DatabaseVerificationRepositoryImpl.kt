90520e5a1785bf5f5b5fb73ab0ced518c7f9949c
nowiwr01w <nowiwr01p@icloud.com>
2024-05-17 00:04:33 +0400

    [Verification, App] Set user verified if verification succeed

--- database/src/main/java/nowiwr01p/daily/doctor/database/repository/verification/DatabaseVerificationRepositoryImpl.kt
+++ database/src/main/java/nowiwr01p/daily/doctor/database/repository/verification/DatabaseVerificationRepositoryImpl.kt
@@ -5,8 +5,11 @@ import com.nowiwr01p.model.api.request.verification.SendVerificationCodeRequest
 import com.nowiwr01p.model.api.response.verification.CheckVerificationCodeResponse
 import com.nowiwr01p.model.api.response.verification.SendVerificationCodeResponse
 import com.nowiwr01p.model.repository.BaseRepository
+import nowiwr01p.daily.doctor.database.table.user.UserEntity
+import nowiwr01p.daily.doctor.database.table.user.UserTable
 import nowiwr01p.daily.doctor.database.table.verification.VerificationCodeEntity
 import nowiwr01p.daily.doctor.database.table.verification.VerificationCodeTable
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.transactions.transaction
 
 class DatabaseVerificationRepositoryImpl: BaseRepository(), DatabaseVerificationRepository {
@@ -24,11 +27,10 @@ class DatabaseVerificationRepositoryImpl: BaseRepository(), DatabaseVerification
         val insertedVerificationCode = VerificationCodeEntity.new {
             email = request.email
             timestamp = request.timestamp
-            code = "123456"
-//            code = "1234567890".toList().shuffled().joinToString().take(4)
+            code = "1234567890".toList().shuffled().joinToString(separator = "").take(6) // TODO: Move to a separated class
         }
         SendVerificationCodeResponse(
-            id = insertedVerificationCode.id.toString()
+            id = insertedVerificationCode.id.toString() // TODO: Change to VerificationToken
         )
     }
 
@@ -40,8 +42,11 @@ class DatabaseVerificationRepositoryImpl: BaseRepository(), DatabaseVerification
             buildError("We somewhat can't process your verification. Please, try again.")
         } else {
             if (request.code == lastSentCode.code) {
-                val revokeSessionTimestamp = System.currentTimeMillis() + 10 * 60 * 1000 // TODO: Change to AuthToken
-                CheckVerificationCodeResponse(revokeSessionTimestamp = revokeSessionTimestamp)
+                setUserVerified(request.email)
+                deleteUserCodes(request.email)
+                CheckVerificationCodeResponse(
+                    revokeSessionTimestamp = System.currentTimeMillis() + 10 * 60 * 1000 // TODO: Change to AuthToken
+                )
             } else {
                 buildError("Wrong code.")
             }
@@ -51,4 +56,9 @@ class DatabaseVerificationRepositoryImpl: BaseRepository(), DatabaseVerification
     private fun deleteUserCodes(email: String) = VerificationCodeEntity
         .find { VerificationCodeTable.email eq email }
         .onEach { it.delete() }
+
+    private fun setUserVerified(email: String) = UserEntity
+        .findSingleByAndUpdate(UserTable.email eq email) { entity ->
+            entity.isEmailVerified = true
+        }
 }
