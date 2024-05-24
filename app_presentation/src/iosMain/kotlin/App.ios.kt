3908451791508a6e006ab05baf704624f1418822
nowiwr01w <nowiwr01p@icloud.com>
2024-05-24 13:29:50 +0400

    [App] Run iosApp configuration

--- app_presentation/src/iosMain/kotlin/App.ios.kt
+++ app_presentation/src/iosMain/kotlin/App.ios.kt
@@ -1,10 +1,15 @@
 import androidx.compose.runtime.Composable
 import com.arkivanov.decompose.ComponentContext
+import com.nowiwr01p.model.di.initKoin
+import di.appModules
 
 @Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 actual class App {
     @Composable
     actual fun initApplicationUI(context: ComponentContext) {
+        startKoin()()
         App(context)
     }
+
+    private fun startKoin(): @Composable () -> Unit = { initKoin(appModules) }
 }
